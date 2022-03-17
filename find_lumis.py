83c994ecdd5e77e54435a2d0c3541309ac587db2
kawaho <kho2@nd.edu>
2022-03-17 12:33:38 -0500

    herwig

--- find_lumis.py
+++ find_lumis.py
@@ -4,14 +4,29 @@ datadefs = {}
 
 #https://twiki.cern.ch/twiki/bin/view/LHCPhysics/CERNYellowReportPageAt13TeV
 #https://twiki.cern.ch/twiki/bin/view/LHCPhysics/CERNHLHE2019
+#https://twiki.cern.ch/twiki/bin/view/LHCPhysics/CERNYellowReportPageBSMAt13TeV
 #m_{H} = 125.09GeV
+datadefs['GluGlu_LFV_HToEMu_M110'] = 52.68*0.01
+datadefs['VBF_LFV_HToEMu_M110'] = 4.434*0.01
+datadefs['GluGlu_LFV_HToEMu_M120'] = 52.22*0.01
+datadefs['VBF_LFV_HToEMu_M120'] = 3.935*0.01
 datadefs['GluGlu_LFV_HToEMu_M125'] = 48.61*0.01
 datadefs['VBF_LFV_HToEMu_M125'] = 3.766*0.01
-datadefs['GluGlu_LFV_HToEMu_M120'] = datadefs['GluGlu_LFV_HToEMu_M125']
-datadefs['GluGlu_LFV_HToEMu_M130'] = datadefs['GluGlu_LFV_HToEMu_M125']
-datadefs['VBF_LFV_HToEMu_M120'] = datadefs['VBF_LFV_HToEMu_M125']
-datadefs['VBF_LFV_HToEMu_M130'] = datadefs['VBF_LFV_HToEMu_M125']
-datadefs['herwig7'] = datadefs['VBF_LFV_HToEMu_M125']
+datadefs['GluGlu_LFV_HToEMu_M130'] = 45.31*0.01
+datadefs['VBF_LFV_HToEMu_M130'] = 3.637*0.01
+datadefs['GluGlu_LFV_HToEMu_M140'] = 34.28*0.01
+datadefs['VBF_LFV_HToEMu_M140'] = 3.492*0.01
+datadefs['GluGlu_LFV_HToEMu_M150'] = 30.29*0.01
+datadefs['VBF_LFV_HToEMu_M150'] = 3.239*0.01
+datadefs['GluGlu_LFV_HToEMu_M160'] = 26.97*0.01
+datadefs['VBF_LFV_HToEMu_M160'] = 3.010*0.01
+datadefs['VBF_LFV_HToEMu_M110H'] = datadefs['VBF_LFV_HToEMu_M110']
+datadefs['VBF_LFV_HToEMu_M120H'] = datadefs['VBF_LFV_HToEMu_M120']
+datadefs['VBF_LFV_HToEMu_M125H'] = datadefs['VBF_LFV_HToEMu_M125']
+datadefs['VBF_LFV_HToEMu_M130H'] = datadefs['VBF_LFV_HToEMu_M130']
+datadefs['VBF_LFV_HToEMu_M140H'] = datadefs['VBF_LFV_HToEMu_M140']
+datadefs['VBF_LFV_HToEMu_M150H'] = datadefs['VBF_LFV_HToEMu_M150']
+datadefs['VBF_LFV_HToEMu_M160H'] = datadefs['VBF_LFV_HToEMu_M160']
 
 datadefs['GluGluHToTauTau'] = 48.61*6.256e-02
 datadefs['VBFHToTauTau'] = 3.766*6.256e-02
@@ -37,13 +52,13 @@ datadefs['DYJetsToLL_M-10to50'] = 18610.0 #15890.0+-24.94 RunIISummer20UL16MiniA
 #NLO 
 #61526.7 NNLO Htt
 #https://twiki.cern.ch/twiki/bin/viewauth/CMS/HowToGenXSecAnalyzer#Running_the_GenXSecAnalyzer_on_a
-#datadefs['WJetsToLNu_TuneCP5'] = 53870.0 #TODO GenXSecAnalyzer
+datadefs['WJetsToLNu_TuneCP5'] = 61526.7 #TODO GenXSecAnalyzer
 datadefs['WJetsToLNu_0J'] = 53330.0 #+-90.89 RunIISummer20UL16MiniAOD-106X_mcRun2_asymptotic_v13-v2 NLO
 datadefs['WJetsToLNu_1J'] = 8875.0 #+-55.31 RunIISummer20UL16MiniAOD-106X_mcRun2_asymptotic_v13-v1 NLO
 datadefs['WJetsToLNu_2J'] = 3338.0 #+-34.64 RunIISummer20UL16MiniAOD-106X_mcRun2_asymptotic_v13-v1 NLO
 
 #LO
-datadefs['WJetsToLNu_TuneCP5'] = 53870.0 #+-129.7 RunIISummer20UL16MiniAOD-106X_mcRun2_asymptotic_v13-v2
+#datadefs['WJetsToLNu_TuneCP5'] = 53870.0 #+-129.7 RunIISummer20UL16MiniAOD-106X_mcRun2_asymptotic_v13-v2
 datadefs['W1JetsToLNu'] = 8927.0 #+-24.09 RunIISummer20UL16MiniAOD-106X_mcRun2_asymptotic_v13-v1
 datadefs['W2JetsToLNu'] = 2809.0 #+-8.201 RunIISummer20UL16MiniAOD-106X_mcRun2_asymptotic_v13-v1
 datadefs['W3JetsToLNu'] = 826.3 #+-2.511 RunIISummer20UL16MiniAOD-106X_mcRun2_asymptotic_v13-v1 
@@ -80,52 +95,64 @@ datadefs['EWKZ2Jets_ZToNuNu'] = 10.66 #+-2.969e-03 GenXSecAnalyzer
 datadefs['EWKWMinus2Jets'] = 32.05 #+-0.02492 RunIISummer20UL16MiniAOD-106X_mcRun2_asymptotic_v13-v2 LO
 datadefs['EWKWPlus2Jets'] = 39.05 #+-0.0291 RunIISummer20UL16MiniAOD-106X_mcRun2_asymptotic_v13-v2 LO
 
-DYNNLO = 6077.22/datadefs['DYJetsToLL_M-50']
-WNNLO = 61526.7/datadefs['WJetsToLNu_TuneCP5']
+#DYNNLO = 6077.22/datadefs['DYJetsToLL_M-50']
+#WNNLO = 61526.7/datadefs['WJetsToLNu_TuneCP5']
 dyLumi, wLumi = 1, 1
 
+#samples_to_find = ['GluGlu_LFV_HToEMu_M125', 'GluGlu_LFV_HToEMu_M120', 'GluGlu_LFV_HToEMu_M130']
+
+samples_to_find = ['GluGlu_LFV_HToEMu_M125', 'VBF_LFV_HToEMu_M125_TuneCP5', 'VBF_LFV_HToEMu_M125_TuneCH3', 'TTTo2L2Nu', 'GluGlu_LFV_HToEMu_M120', 'VBF_LFV_HToEMu_M120_TuneCP5', 'VBF_LFV_HToEMu_M120_TuneCH3', 'GluGlu_LFV_HToEMu_M130', 'VBF_LFV_HToEMu_M130_TuneCP5', 'VBF_LFV_HToEMu_M130_TuneCH3', 'ZZ_TuneCP5', 'WZ_TuneCP5', 'WW_TuneCP5', 'DYJetsToLL_M-50', 'DYJetsToLL_M-10to50', 'WGToLNuG_TuneCP5', 'TTToSemiLeptonic', 'TTToHadronic', 'ST_tW_antitop_5f_inclusiveDecays', 'ST_tW_top_5f_inclusiveDecays', 'ST_t-channel_antitop_4f_InclusiveDecays', 'ST_t-channel_top_4f_InclusiveDecays', 'EWKZ2Jets_ZToLL', 'EWKZ2Jets_ZToNuNu', 'EWKWMinus2Jets', 'EWKWPlus2Jets', 'GluGluHToTauTau', 'VBFHToTauTau', 'WminusHToTauTau', 'WplusHToTauTau', 'GluGluHToWWTo2L2Nu', 'VBFHToWWTo2L2Nu', 'ZHToTauTau', 'WJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-pythia8']
 
 def mclumi(sample):
-  for i in datadefs:
-    if i in sample:
-      return i, datadefs[i]
-  print("wrong", sample)
-  return -1, -1
+  lumi_tmp = datadefs.get(sample)
+  if lumi_tmp is None:
+    print("wrong", sample)
+    return -1, -1
+  return sample, lumi_tmp
 
 if __name__ == '__main__':
-  datalumis = {'2016preVFP': 19514.7, '2016postVFP': 16810.8, '2017': 41476.1, '2018': 59817.3}
-  for year in ['2016postVFP', '2017', '2018']: #['2016preVFP']:#datalumis:
-    samples_names = glob.glob('/hdfs/store/user/kaho/NanoPost_'+year+'_v2/*')
+  datalumis = {'2016preVFP': 19514.7}#, '2016postVFP': 16810.8, '2017': 41476.1, '2018': 59817.3}
+  for year in datalumis:
+#    samples_names = glob.glob('/hdfs/store/user/kaho/NanoPost_'+year+'_v2/*')
     sample_paths = {}
     lumiWeight = {}
-    for name in samples_names:
-       print(f"Running sample {name}")
-       if 'SingleMuon' in name or 'tmp' in name or 'WJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-pythia8' in name: continue
-       sample_basename = os.path.basename(name)
-       sample_paths[sample_basename] = glob.glob(name+'/*/*/*/*root')
+    for name in samples_to_find:
+       #if 'SingleMuon' in name or 'tmp' in name or 'WJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-pythia8' in name: continue
+       sample_basename = name
+       if 'VBF_LFV' in sample_basename:
+         sample_basename = sample_basename.replace('_TuneCH3','H').replace('_TuneCP5','')#os.path.basename(name)
+       if 'WJetsToLNu_TuneCP5' in sample_basename:
+         sample_basename = sample_basename.split('-')[0].replace('_13TeV','')
+       print(f"Running sample {sample_basename}")
+       sample_paths[sample_basename] = glob.glob('/hdfs/store/user/kaho/NanoPost_'+year+'_v2/'+name+'*/*/*/*/*root')
+       if len(sample_paths[sample_basename]) == 0:
+         print(f"sample {name} is empty")
+         continue
        lumiWeight[mclumi(sample_basename)[0]] = 0
        runTrees = [i+':Runs' for i in sample_paths[sample_basename]]
        for runTree in uproot.iterate(runTrees, ['genEventSumw'], num_workers=12):
          lumiWeight[mclumi(sample_basename)[0]]+=sum(runTree['genEventSumw']) 
   
+    print(lumiWeight)
     for sample_basename in lumiWeight:
-      if (re.match(re.compile('W.Jet'), sample_basename)) or "WJetsToLNu_TuneCP5" in sample_basename: 
-      #if ('DY' in sample_basename and not '10to50' in sample_basename) or (re.match(re.compile('W.Jet'), sample_basename)) or "WJetsToLNu_TuneCP5" in sample_basename: 
-        print(f"{sample_basename} is DYMll50 or W+Jets")
-        lumiWeight[sample_basename] = lumiWeight[sample_basename]/(mclumi(sample_basename)[1])
-        if sample_basename=="DYJetsToLL_M-50":
-          dyLumi = lumiWeight[sample_basename]
-        if sample_basename=="WJetsToLNu_TuneCP5":
-          wLumi = lumiWeight[sample_basename]
+#      if (re.match(re.compile('W.Jet'), sample_basename)) or "WJetsToLNu_TuneCP5" in sample_basename: 
+#      #if ('DY' in sample_basename and not '10to50' in sample_basename) or (re.match(re.compile('W.Jet'), sample_basename)) or "WJetsToLNu_TuneCP5" in sample_basename: 
+#        print(f"{sample_basename} is DYMll50 or W+Jets")
+#        lumiWeight[sample_basename] = lumiWeight[sample_basename]/(mclumi(sample_basename)[1])
+#        if sample_basename=="DYJetsToLL_M-50":
+#          dyLumi = lumiWeight[sample_basename]
+#        if sample_basename=="WJetsToLNu_TuneCP5":
+#          wLumi = lumiWeight[sample_basename]
+#
+#      else:
+      lumiWeight[sample_basename] = mclumi(sample_basename)[1]*datalumis[year]/lumiWeight[sample_basename]  
+      print(sample_basename, mclumi(sample_basename)[1])
 
-      else:
-        lumiWeight[sample_basename] = mclumi(sample_basename)[1]*datalumis[year]/lumiWeight[sample_basename]  
-
-    for sample_basename in lumiWeight:
-      if "WJetsToLNu_TuneCP5" in sample_basename: 
-        lumiWeight[sample_basename] = WNNLO*datalumis[year]/wLumi
-      elif re.match(re.compile('W.Jet'), sample_basename): 
-        lumiWeight[sample_basename] = WNNLO*datalumis[year]/(lumiWeight[sample_basename]+wLumi) 
+    #for sample_basename in lumiWeight:
+    #  if "WJetsToLNu_TuneCP5" in sample_basename: 
+    #    lumiWeight[sample_basename] = WNNLO*datalumis[year]/wLumi
+    #  elif re.match(re.compile('W.Jet'), sample_basename): 
+    #    lumiWeight[sample_basename] = WNNLO*datalumis[year]/(lumiWeight[sample_basename]+wLumi) 
       #elif "DYJetsToLL_M-50" in sample_basename:
       #  lumiWeight[sample_basename] = DYNNLO*datalumis[year]/dyLumi
       #elif "DY" in sample_basename and not "M-10to50" in sample_basename:
